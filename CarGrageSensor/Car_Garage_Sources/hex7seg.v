df981c36c4b37872f594538fb11ebad7cbac5ee5
RazWerner <Razwerner65@gmail.com>
2025-05-15 11:03:55 -0700

    Update hex7seg.v

--- CarGrageSensor/Car_Garage_Sources/hex7seg.v
+++ CarGrageSensor/Car_Garage_Sources/hex7seg.v
@@ -22,6 +22,7 @@ module hex7seg(
     input [3:0] n,
     output [6:0] seg
 );
+    //logic for each segment of 7 segment display converts hex to 7 segment readable numbers and letters
     assign seg[0] = (~n[3] & ~n[2] & ~n[1] & n[0]) | (~n[3] & n[2] & ~n[1] & ~n[0]) | (n[3] & ~n[2] & n[1] & n[0]) | (n[3] & n[2] & ~n[1] & n[0]);
     assign seg[1] = (~n[3] & n[2] & ~n[1] & n[0]) | (~n[3] & n[2] & n[1] & ~n[0]) | (n[3] & ~n[2] & n[1] & n[0]) | (n[3] & n[2] & ~n[1] & ~n[0]) | (n[3] & n[2] & n[1] & ~n[0]) | (n[3] & n[2] & n[1] & n[0]);
     assign seg[2] = (~n[3] & ~n[2] & n[1] & ~n[0]) | (n[3] & n[2] & ~n[1] & ~n[0]) | (n[3] & n[2] & n[1] & ~n[0]) | (n[3] & n[2] & n[1] & n[0]);
